2b1e11b3ad935535e6e321fb18d10ec8c1a0641f
Nick <ptnguyen1901@gmail.com>
2020-07-26 16:32:20 -0400

    Initial Commit

---
+++ FoodDeliverySwiftUI/FoodDeliverySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliverySwiftUIApp.swift
+//  FoodDeliverySwiftUI
+//
+//  Created by Nick Nguyen on 7/26/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliverySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
